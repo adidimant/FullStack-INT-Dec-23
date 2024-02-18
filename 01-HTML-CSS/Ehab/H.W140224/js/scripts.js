f54e2732a2b395f1abfebb9c5972c96ded4c596a
asonhas <asonhas2@gmail.com>
2024-02-18 18:43:58 +0200

    HW110224

--- 01-HTML-CSS/Ehab/H.W140224/js/scripts.js
+++ 01-HTML-CSS/Ehab/H.W140224/js/scripts.js
@@ -10,7 +10,7 @@ function OpenOrClose(articleId){
     }
 }
 
-var hasScrolled40Percent = false;
+//var hasScrolled40Percent = false;
 window.addEventListener('scroll', function() {
     // Get the height of the window
     var windowHeight = window.innerHeight || this.window.pageYOffset || document.documentElement.scrollTop;
@@ -19,7 +19,7 @@ window.addEventListener('scroll', function() {
     // Calculate the percentage scrolled
     var scrollPercentage = (scrollPosition / (document.documentElement.scrollHeight - windowHeight)) * 100;
     // Check if the user has scrolled 40% down the page
-    if (scrollPercentage >= 40 && !hasScrolled40Percent) {
+    if (scrollPercentage >= 40 /*&& !hasScrolled40Percent*/) {
         if(document.getElementById('article1').classList.contains('articleClose') && 
         document.getElementById('article2').classList.contains('articleClose') &&
         document.getElementById('article3').classList.contains('articleClose')){
@@ -30,9 +30,9 @@ window.addEventListener('scroll', function() {
             hasScrolled40Percent = true;
         }
     }
-    if (scrollPercentage < 40){
+    /*if (scrollPercentage < 40){
         hasScrolled40Percent = false;
-    }
+    }*/
 });
 
 function controlWindowHide(){
