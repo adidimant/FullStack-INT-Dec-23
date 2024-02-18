2630bee986df2ede23d9127f41485e1c6b1a564d
asonhas <asonhas2@gmail.com>
2024-02-18 20:44:48 +0200

    HW110224

--- 01-HTML-CSS/Ehab/H.W140224/js/scripts.js
+++ 01-HTML-CSS/Ehab/H.W140224/js/scripts.js
@@ -10,10 +10,7 @@ function OpenOrClose(articleId){
     }
 }
 
-<<<<<<< HEAD
-=======
 //var hasScrolled40Percent = false;
->>>>>>> 55fdcb79f80e80fbb389c63193aad2d95f909717
 window.addEventListener('scroll', function() {
     // Get the height of the window
     var windowHeight = window.innerHeight || this.window.pageYOffset || document.documentElement.scrollTop;
@@ -22,11 +19,7 @@ window.addEventListener('scroll', function() {
     // Calculate the percentage scrolled
     var scrollPercentage = (scrollPosition / (document.documentElement.scrollHeight - windowHeight)) * 100;
     // Check if the user has scrolled 40% down the page
-<<<<<<< HEAD
     if (scrollPercentage >= 40) {
-=======
-    if (scrollPercentage >= 40 /*&& !hasScrolled40Percent*/) {
->>>>>>> 55fdcb79f80e80fbb389c63193aad2d95f909717
         if(document.getElementById('article1').classList.contains('articleClose') && 
         document.getElementById('article2').classList.contains('articleClose') &&
         document.getElementById('article3').classList.contains('articleClose')){
@@ -36,12 +29,6 @@ window.addEventListener('scroll', function() {
             document.body.classList.add('freeze-scrolling');
         }
     }
-<<<<<<< HEAD
-=======
-    /*if (scrollPercentage < 40){
-        hasScrolled40Percent = false;
-    }*/
->>>>>>> 55fdcb79f80e80fbb389c63193aad2d95f909717
 });
 
 function controlWindowHide(){
