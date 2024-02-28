ab23d0e0f75f40241d0fcb60c04c14e8d92e7e43
Benjamin <benjaminjaichenco@gmail.com>
2024-02-28 11:47:34 +0200

    create showBrandModels function

---
+++ 01-HTML-CSS/beni/html-css project/scripts/rent-form-script.js
@@ -0,0 +1,58 @@
+const rollsRoyceModels = ["Phantom", "Ghost", "Cullinan"];
+const alfaRomeoModels = ["Stelvio", "Giulia", "Tonale"];
+const astonMartinModels = ["Valhalla", "Valour", "DBS Coupe"];
+const mercedesModels = ["Maybach", "C-Class", "G-Class"];
+const ferrariModels = ["LaFerrari", "SF90 Spider", "812 GTS"];
+const porscheModels = ["911 Carrera", "911 Turbo S", "718 Spyder RS"];
+const bmwModels = ["i8", "X6 M Coupe", "M8 Competition"];
+
+const brandSelectInput = document.getElementById("brand");
+const modelSelectInput = document.getElementById("model");
+
+function showBrandModels() {
+  console.log("changed");
+  value = brandSelectInput.value;
+  if (value == "rolls-royce") {
+    modelSelectInput.innerHTML = `<option value="" hidden="" disabled="" selected="">Choose car model</option>`;
+    rollsRoyceModels.forEach((model) => {
+      createOptions(model);
+    });
+  } else if (value == "alfa-romeo") {
+    modelSelectInput.innerHTML = `<option value="" hidden="" disabled="" selected="">Choose car model</option>`;
+    alfaRomeoModels.forEach((model) => {
+      createOptions(model);
+    });
+  } else if (value == "aston-martin") {
+    modelSelectInput.innerHTML = `<option value="" hidden="" disabled="" selected="">Choose car model</option>`;
+    astonMartinModels.forEach((model) => {
+      createOptions(model);
+    });
+  } else if (value == "mercedes-benz") {
+    modelSelectInput.innerHTML = `<option value="" hidden="" disabled="" selected="">Choose car model</option>`;
+    mercedesModels.forEach((model) => {
+      createOptions(model);
+    });
+  } else if (value == "ferrari") {
+    modelSelectInput.innerHTML = `<option value="" hidden="" disabled="" selected="">Choose car model</option>`;
+    ferrariModels.forEach((model) => {
+      createOptions(model);
+    });
+  } else if (value == "porsche") {
+    modelSelectInput.innerHTML = `<option value="" hidden="" disabled="" selected="">Choose car model</option>`;
+    porscheModels.forEach((model) => {
+      createOptions(model);
+    });
+  } else if (value == "bmw") {
+    modelSelectInput.innerHTML = `<option value="" hidden="" disabled="" selected="">Choose car model</option>`;
+    bmwModels.forEach((model) => {
+      createOptions(model);
+    });
+  }
+}
+
+function createOptions(model) {
+  let option = document.createElement("option");
+  option.value = model;
+  option.textContent = model;
+  modelSelectInput.appendChild(option);
+}
