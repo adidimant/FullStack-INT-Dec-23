c9163561f1888f2bceea95a9281350257af74989
adidimant <adi.dimant@transmitsecurity.com>
2024-03-06 20:35:37 +0200

    Add query selector content

--- 02-Javascript/querySelectors-vs-getElements.js
+++ 02-Javascript/querySelectors-vs-getElements.js
@@ -14,3 +14,19 @@ document.getElementsByTagName('div') // gets all the divs in the document
 document.querySelectorAll('div')[2] // gets all the divs then chose the 3rd div from the list
 document.getElementsByTagName('div')[2] // gets all the divs then chose the 3rd div from the list
 
+const container = document.querySelector("#test"); // gets the element with the id="test" 
+const matches = container.querySelectorAll("div.highlighted > p"); // get a list of <p> elements whose immediate parent element is a <div> with the class highlighted and which are located inside a container whose ID is test.
+const matches2 = container.querySelectorAll("div.highlighted.some-class .some-class2 p"); // get a list of <p> elements whose inside an element with the class 'some-class2' which is an eement inside a div that its class is highlighted & some-class ,and which are located inside a container whose ID is test.
+
+//Here, an attribute selector is used to return a list of the list items contained within a list whose ID is userlist which have a data-active attribute whose value is 1:
+const container2 = document.querySelector("#userlist");
+const matches3 = container2.querySelectorAll("li[data-active='1']");
+
+a.forEach((num) => {
+  console.log(num+1);
+});
+
+const divs = document.querySelectorAll('div');
+divs.forEach((div, index) => {
+    console.log('div at index: ' + index, div.id);
+});
