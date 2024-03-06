fcd12edfecc347decd6c6e61bf01a13462bc9fdf
Benjamin <benjaminjaichenco@gmail.com>
2024-03-06 19:14:15 +0200

    submit project v1

--- 01-HTML-CSS/beni/html-css project/scripts/index-script.js
+++ 01-HTML-CSS/beni/html-css project/scripts/index-script.js
@@ -1,23 +1,34 @@
 const iframe = document.querySelector("iframe");
-const rent = document.querySelector('a[href="./rent-form.html"]');
+const rent = document.querySelector('a[href="./pages/rent-form.html"]');
 
 function toggleNavItem(item) {
-  console.log(item);
   document.querySelectorAll(".nav-link").forEach((link) => {
     link.classList.remove("active");
   });
+  if (item.textContent == "ARISTEIA") {
+    document
+      .querySelector('nav a[href="./pages/home.html"]')
+      .classList.add("active");
+  }
   item.classList.add("active");
+  if (item.textContent == "Rent") {
+    iframe.onload = function () {
+      const brandOptions =
+        iframe.contentDocument.querySelectorAll("#brand option");
+      brandOptions[0].selected = "selected";
+    };
+  } else {
+    iframe.onload = "";
+  }
   if (item.textContent == "Catalog") {
     iframe.onload = function () {
       const cars = iframe.contentDocument.querySelectorAll(".car-container");
       cars.forEach((car) => {
         car.onclick = function () {
-          console.log(this.querySelector(".brand-name").textContent);
           const brandName = this.querySelector(".brand-name").textContent;
           const modelName = this.querySelector(".model-name").textContent;
-          console.log(modelName);
           toggleNavItem(rent);
-          iframe.src = "./rent-form.html";
+          iframe.src = "./pages/rent-form.html";
           iframe.onload = function () {
             const brandInput = iframe.contentDocument.getElementById("brand");
             const modelInput = iframe.contentDocument.getElementById("model");
@@ -29,5 +40,7 @@ function toggleNavItem(item) {
         };
       });
     };
+  } else {
+    iframe.onload = "";
   }
 }
