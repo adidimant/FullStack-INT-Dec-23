f4862a319b1b0993699325ada4ffcf3faa68e94b
asonhas <asonhas2@gmail.com>
2024-03-08 02:29:31 +0200

    08032024

--- 01-HTML-CSS/Ehab/html-css project/js/main/scripts.js
+++ 01-HTML-CSS/Ehab/html-css project/js/main/scripts.js
@@ -1,7 +1,7 @@
-setInterval(galleryspin, 2000);
+/*setInterval(galleryspin, 2000);
 var angle = 0;
 function galleryspin() { 
     spinner = document.querySelector("#spinner");
     angle = angle + 45;
     spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-}
+}*/
