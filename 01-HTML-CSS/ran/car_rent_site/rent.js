422fb3dd8d14c6abd142c3e949eb5dfaef007e2f
nishku84 <93193209+nishku84@users.noreply.github.com>
2024-03-12 12:08:11 +0000

    12 במרץ 2024, 14:08

--- 01-HTML-CSS/ran/car_rent_site/rent.js
+++ 01-HTML-CSS/ran/car_rent_site/rent.js
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
         'company3': ['brz']
     };
 
-    // עדכון דינמי של רשימת הדגמים
+    
     companySelect.addEventListener('change', function () {
         const models = carModels[this.value];
-        modelSelect.innerHTML = '<option value="">בחר דגם...</option>'; // ניקוי רשימת הדגמים
+        modelSelect.innerHTML = '<option value="">בחר דגם...</option>'; 
 
         if (models) {
             models.forEach(function (model) {
@@ -23,14 +23,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // אימות כתובת אימייל
+    
     form.addEventListener('submit', function (event) {
         const emailValue = emailInput.value;
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         if (!emailPattern.test(emailValue)) {
             alert('נא להזין כתובת אימייל תקינה.');
-            event.preventDefault(); // מניעת השליחה של הטופס
+            event.preventDefault(); 
         }
     });
 });
