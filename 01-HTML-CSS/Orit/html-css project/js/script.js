ca0264e4d0e84cfe66644409e2b957116452ab0d
oritbg <oritbg@gmail.com>
2024-03-14 02:05:54 +0200

    Final files of project

--- 01-HTML-CSS/Orit/html-css project/js/script.js
+++ 01-HTML-CSS/Orit/html-css project/js/script.js
@@ -1,3 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current URL
+    var currentUrl = window.location.pathname;
+
+    // Get all navigation links
+    var navLinks = document.querySelectorAll('nav ul li a');
+
+    // Loop through each navigation link
+    navLinks.forEach(function(link) {
+        // Check if the link's href matches the current URL
+        if (currentUrl.includes(link.parentNode.getAttribute('data-nav'))) {
+        //if (link.getAttribute('href').includes(link.parentNode.getAttribute('data-nav'))) {
+            // Add the 'active' class to the parent <li> element
+            link.parentNode.classList.add('active');
+        }
+    });
+});
+
 function passToForm(event) {
     var parentDiv = event.target.closest('.car');
         
@@ -10,3 +29,5 @@ function passToForm(event) {
     localStorage.setItem("carData", JSON.stringify(data));
     window.location.href = "rent.html";
 }
+
+
