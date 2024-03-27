00c987f1d977c91846e9e1099c1c0bc8f2f4264d
nishku84 <93193209+nishku84@users.noreply.github.com>
2024-03-27 15:59:09 +0000

    27 במרץ 2024, 17:59

--- 02-Javascript/HW/240324/ran/script.js
+++ 02-Javascript/HW/240324/ran/script.js
@@ -5,7 +5,7 @@ function filterOddNumbersFromArray(array) {
 
 const originalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const oddNumbersArray = filterOddNumbersFromArray(originalArray);
-console.log(oddNumbersArray); // ידפיס: [1, 3, 5, 7, 9]
+console.log(oddNumbersArray); 
 
 
 function containsObject(arr) {
