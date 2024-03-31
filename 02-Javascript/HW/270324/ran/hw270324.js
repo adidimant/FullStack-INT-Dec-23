81b1e39e84bb01861b104e8c74617cb5330556a4
nishku84 <93193209+nishku84@users.noreply.github.com>
2024-03-31 13:41:01 +0000

    31 במרץ 2024, 16:41

--- 02-Javascript/HW/270324/ran/hw270324.js
+++ 02-Javascript/HW/270324/ran/hw270324.js
@@ -32,3 +32,31 @@ const reversedArray = reverse(originalArray);
 console.log("מערך מקורי:", originalArray); // פלט: מערך מקורי: [1, 2, 3, 4, 5]
 console.log("מערך הפוך:", reversedArray); // פלט: מערך הפוך: [5, 4, 3, 2, 1]
 
+3.
+function arrayToObject(array) {
+    return array.reduce((obj, current, index) => {
+      obj[index] = current;
+      return obj;
+    }, {});
+  }
+  
+  // דוגמה ראשונה: מערך של מחרוזות
+  const fruits = ['apple', 'banana', 'cherry'];
+  console.log(arrayToObject(fruits)); // פלט: {0: 'apple', 1: 'banana', 2: 'cherry'}
+  
+  // דוגמה שנייה: מערך של מספרים
+  const numbers = [10, 20, 30, 40, 50];
+  console.log(arrayToObject(numbers)); // פלט: {0: 10, 1: 20, 2: 30, 3: 40, 4: 50}
+  
+  // דוגמה שלישית: מערך הכולל מחרוזות ומספרים
+  const mixedArray = ['first', 2, 'third', 4];
+  console.log(arrayToObject(mixedArray)); // פלט: {0: 'first', 1: 2, 2: 'third', 3: 4}
+  
+  // דוגמה רביעית: מערך של בוליאנים
+  const booleans = [true, false, true];
+  console.log(arrayToObject(booleans)); // פלט: {0: true, 1: false, 2: true}
+  
+  // דוגמה חמישית: מערך של אובייקטים
+  const objectsArray = [{name: 'John'}, {age: 30}, {isStudent: false}];
+  console.log(arrayToObject(objectsArray)); // פלט: {0: {name: 'John'}, 1: {age: 30}, 2: {isStudent: false}}
+  
