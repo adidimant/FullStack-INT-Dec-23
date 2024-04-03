cd3c06d246f785940ad5e43e8a8fb66f7707b6ec
nishku84 <93193209+nishku84@users.noreply.github.com>
2024-04-03 14:45:29 +0000

    3 באפר׳ 2024, 17:45

--- 02-Javascript/ran/hw310324.js
+++ 02-Javascript/ran/hw310324.js
@@ -2,7 +2,7 @@ function filterObjectsWithAtLeastSevenKeys(objectsArray) {
   return objectsArray.filter(object => Object.keys(object).length >= 7);
 }
 
-// Example usage:
+
 const objectsArray = [
   { name: 'John', age: 25, city: 'New York', occupation: 'Engineer', email: 'john@example.com', phone: '123-456-7890', hobby: 'Reading' },
   { name: 'Jane', age: 28, city: 'Los Angeles', occupation: 'Designer' },
@@ -13,3 +13,24 @@ const objectsArray = [
 const filteredObjects = filterObjectsWithAtLeastSevenKeys(objectsArray);
 
 console.log(filteredObjects);
+
+//2.//
+
+const users = [
+  { fullName: "John Doe", id: 1, password: "123456!", age: 24 },
+  { fullName: "Jane Smith", id: 2, password: "password", age: 30 },
+  { fullName: "Jack White", id: 3, password: "pass123@", age: 22 },
+  { fullName: "Jill Green", id: 4, password: "1234", age: 27 },
+  { fullName: "Joe Black", id: 5, password: "1234567$", age: 20 }
+];
+
+const usersSimplified = users.map(user => ({
+  fullName: user.fullName,
+  id: user.id
+}));
+
+
+const complexPasswordUsers = users.filter(user => user.password.length >= 6 && /[!@#$%^&*(),.?":{}|<>]/g.test(user.password));
+
+
+const youngest = users.reduce((youngestSoFar, user) => user.age < youngestSoFar.age ? user : youngestSoFar, users[0]);
