e46a77f52890a57bcb51534b342403f0fab9e260
lorenabarlev <lorenabarlev@gmail.com>
2024-04-08 14:34:29 +0300

    promises HW update

---
+++ 02-Javascript/Lorena/drafts/substrings/subbstrings.js
@@ -0,0 +1,25 @@
+let str = "lulibarlev";
+let ss = str.substring(2, 1);
+console.log(ss);
+
+
+
+let origStr = 'lorena';
+let firstPart = origStr.substring(o, 2);
+let secondPart;
+if (orgStr[2]=== '0' ||
+    orgStr[2]=== '1' ||
+    orgStr[2]=== '2' ||
+    orgStr[2]=== '3' ||
+    orgStr[2]=== '4' ||
+    orgStr[2]=== '5' ||
+    orgStr[2]=== '6' ||
+    orgStr[2]=== '7' ||
+    orgStr[2]=== '8' ||
+    orgStr[2]=== '9')
+    secondPart = '-';
+    else
+    secondPart = '+';
+let thirPart = origStr.substring(3);
+let updateStr = firstPart + secondPart + thirPart;
+console.log(updatedStr);
