1b699b9a144d8d9dad7e46130a26f46e7920014f
oritbg <oritbg@gmail.com>
2024-04-11 10:53:14 +0300

    hw

--- 02-Javascript/Orit/310324/script.js
+++ 02-Javascript/Orit/310324/script.js
@@ -1,36 +1,5 @@
 function ex1() {
-    const users = [
-        {
-            id: 1,
-            name: 'John',
-            city: 'New York',
-            country: 'USA',
-        },
-        {
-            id: 2,
-            name: 'Alice',
-            age: 25,
-            city: 'London',
-            country: 'UK',
-            occupation: 'Designer'
-        },
-        {
-            id: 3,
-            name: 'Michael',
-            age: 35,
-            city: 'Paris',
-            country: 'France',
-            occupation: 'Manager'
-        }
-      ];
-
-      function filterObjectsWithFiveKeys(array) {
-        return array.filter(obj => Object.keys(obj).length >= 5);
-    }
-
-    const filteredArray = filterObjectsWithFiveKeys(users);
-
-    console.log(filteredArray);
+    
 }
 
 function ex2(x) {
@@ -100,3 +69,20 @@ function ex2(x) {
     
 }
 
+/*
+** HW 07/04/24 **
+1) Write a promise of your own with two ways: 1) standard way via new Promise, 2) write an async function
+your promise & async function should contain something we didn't showed in class
+show how you await the promise, gets the resolved value, and how you use .then for executing a function when the promise is resolved.
+
+2) Write a "promise chain" - you need to write 4 different promises, that 1 promise is calling the next one
+when the first promise is resolved - it means that all the promises are resolved
+
+3) Write the function `getPromiseWithStatus`, the function gets a promise, and returns the promise with an extra field with its `status`: "pending"/"fulfilled"/"rejected"
+hint: use the .then,.catch functions on the received promise
+```js
+function getPromiseWithStatus(promise) {
+
+}
+```
+*/
