b831e25677e3591427571e29abeb4425d194f6e5
Ofer Ben Ami <132418948+OferBenA@users.noreply.github.com>
2024-04-17 16:23:32 +0300

    starting from submit continuing on css

--- 02-Javascript/Ofer/JS Unit Project/TS/dist/userView.js
+++ 02-Javascript/Ofer/JS Unit Project/TS/dist/userView.js
@@ -1,1 +1,10 @@
-console.log("test");
+var _a;
+console.log("123");
+function submitForm(event) {
+    event.preventDefault();
+    var elements = event.target.elements;
+    console.log(elements);
+}
+(_a = document.getElementById('myForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from submitting
+});
