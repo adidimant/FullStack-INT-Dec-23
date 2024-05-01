e5b94d2b40e4b85d98f98637e238c186b9d7aa59
Ofer Ben Ami <132418948+OferBenA@users.noreply.github.com>
2024-05-01 13:28:45 +0300

    Upload Finished JS project

--- 02-Javascript/Ofer/JS Unit Project/TS/userCreation.ts
+++ 02-Javascript/Ofer/JS Unit Project/TS/userCreation.ts
@@ -7,7 +7,7 @@
 // } from "./tsFunctions/DBrelatedFunctions";
 // import {formExtension, userDetails} from "./interfaces/interfaces";
 
- interface formExtension extends HTMLFormControlsCollection {
+interface formExtension extends HTMLFormControlsCollection {
     userName: HTMLInputElement;
     email: HTMLInputElement;
     password: HTMLInputElement;
@@ -33,7 +33,7 @@ interface userDetails {
     updatedDate: string;
 }
 
-console.log(`first`)
+console.log(`first`);
 
 //initialize localStorage in case they are not existing
 if (!localStorage.getItem("userID")) {
@@ -47,8 +47,9 @@ if (!localStorage.getItem("userDB")) {
 
 function submitForm(event: SubmitEvent) {
     event.preventDefault();
-    const elements = (event.target as HTMLFormElement).elements as formExtension;
-    console.log(`elements Details: ${elements}`)
+    const elements = (event.target as HTMLFormElement)
+        .elements as formExtension;
+    console.log(`elements Details: ${elements}`);
     const userDetail: userDetails = {
         userName: elements.userName.value,
         email: elements.email.value,
@@ -57,12 +58,12 @@ function submitForm(event: SubmitEvent) {
         firstName: elements.firstName.value,
         lastName: elements.lastName.value,
         country: elements.countryList.value,
-        city: elements.city?.value ,
+        city: elements.city?.value,
         zipCode: elements.zipCode?.value,
         registeredDate: String(Date.now()),
         updatedDate: String(Date.now()),
     };
-    console.log(`user Details: ${userDetail.city} ${userDetail.zipCode}`)
+    console.log(`user Details: ${userDetail.city} ${userDetail.zipCode}`);
 
     const usersIDArrayFromDB = getUserIDFromLocalStorage();
     const usersFromDB = getUserDBFromLocalStorage();
@@ -71,51 +72,123 @@ function submitForm(event: SubmitEvent) {
         alert(
             `Username "${userDetail.userName}" is already taken, please select a different username`
         );
-    }else if(checkIfEmailInDB(usersFromDB,userDetail) ){
+    } else if (checkIfEmailInDB(usersFromDB, userDetail)) {
         alert(
             `Email "${userDetail.email}" is already taken, please select a different Email`
         );
+    } else if (passwordValidation(userDetail.password, specialCharacters)) {
+        alert(
+            `Password is not strong enough, Please make sure it has at least 1 upperCase letter and a special character`
+        );
+    } else if (phoneValidation(userDetail.phoneNumber)) {
+        alert(
+            `phone number is not valid, please make sure it's the correct length and starts with '05'`
+        );
+    } else if (checkIfPhoneInDB(usersFromDB, userDetail)) {
+        alert(
+            `Phone Number "${userDetail.phoneNumber}" is already taken, please select a different Phone Number`
+        );
+    } else {
+        pushUserIDToLocalStorage(usersIDArrayFromDB, userDetail.userName);
+        pushUserDBToLocalStorage(usersFromDB, userDetail);
+        alert(`user "${userDetail.userName}" Created succesfully!`);
+        history.back();
     }
-     else {
-         pushUserIDToLocalStorage(usersIDArrayFromDB,userDetail.userName )
-         pushUserDBToLocalStorage(usersFromDB, userDetail)
-         alert(`user "${userDetail.userName}" Created succesfully!`)
-         history.back()
-    }
-
 }
 
-
- function checkIfUserInDB(usersIDArrayFromDB:string[], newUserID:string){
-    return usersIDArrayFromDB.some((user: string) => user == newUserID)
+function passwordValidation(
+    password: string,
+    specialCharacters: string[]
+): boolean {
+    console.log(password !== password.toLowerCase());
+    return !(
+        password !== password.toLowerCase() &&
+        specialCharacters.some((char) => password.includes(char))
+    );
 }
 
- function checkIfEmailInDB(usersArrayFromDB:userDetails[], newUser:userDetails){
-    return usersArrayFromDB.some((user:userDetails) => user.email === newUser.email)
+function phoneValidation(phoneNum: string): boolean {
+    return !(
+        phoneNum.length == 10 &&
+        !Number.isNaN(phoneNum) &&
+        phoneNum.charAt(0) == "0" &&
+        phoneNum.charAt(1) == "5"
+    );
 }
 
- function getUserIDFromLocalStorage() {
+function checkIfPhoneInDB(
+    usersArrayFromDB: userDetails[],
+    newUser: userDetails
+) {
+    return usersArrayFromDB.some(
+        (user: userDetails) => user.phoneNumber === newUser.phoneNumber
+    );
+}
+
+function checkIfUserInDB(usersIDArrayFromDB: string[], newUserID: string) {
+    return usersIDArrayFromDB.some((user: string) => user == newUserID);
+}
+
+function checkIfEmailInDB(
+    usersArrayFromDB: userDetails[],
+    newUser: userDetails
+) {
+    return usersArrayFromDB.some(
+        (user: userDetails) => user.email === newUser.email
+    );
+}
+
+function getUserIDFromLocalStorage() {
     const usersBefore = localStorage.getItem("userID") ?? "";
     const users = JSON.parse(usersBefore);
     return users;
 }
 
-
- function getUserDBFromLocalStorage() {
+function getUserDBFromLocalStorage() {
     const usersBefore = localStorage.getItem("userDB") ?? "";
     const users = JSON.parse(usersBefore);
     return users;
 }
 
-
- function pushUserIDToLocalStorage(usersArray: string[],newUserID:string) {
+function pushUserIDToLocalStorage(usersArray: string[], newUserID: string) {
     usersArray.push(newUserID);
     const usersToString = JSON.stringify(usersArray);
     localStorage.setItem("userID", usersToString);
 }
 
- function pushUserDBToLocalStorage(usersArray:userDetails[],newUser:userDetails) {
-    usersArray.push(newUser)
-    const usersToString = JSON.stringify(usersArray)
-    localStorage.setItem("userDB" ,usersToString)
+function pushUserDBToLocalStorage(
+    usersArray: userDetails[],
+    newUser: userDetails
+) {
+    usersArray.push(newUser);
+    const usersToString = JSON.stringify(usersArray);
+    localStorage.setItem("userDB", usersToString);
 }
+
+const specialCharacters: string[] = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "-",
+    "`",
+    "~",
+    "{",
+    "}",
+    "'",
+    '"',
+    "/",
+    ",",
+    "+",
+    "=",
+    "_",
+    "|",
+    "[",
+    "]",
+];
