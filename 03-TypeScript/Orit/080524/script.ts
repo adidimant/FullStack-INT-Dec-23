9a4ec8dba8c992594a5907be3aab2b32b9c643c2
oritbg <oritbg@gmail.com>
2024-05-09 03:07:08 +0300

    hw

---
+++ 03-TypeScript/Orit/080524/script.ts
@@ -0,0 +1,51 @@
+enum trafficColor{
+    RED = 'RED',
+    ORANGE = 'ORANGE',
+    GREEN = 'GREEN',
+    REDORANGE = 'REDORANGE'
+}
+const redLight: HTMLElement | null = document.getElementById('red');
+const orangeLight: HTMLElement | null = document.getElementById('orange');
+const greenLight: HTMLElement | null = document.getElementById('green');
+
+async function delay(ms: number): Promise<void> {
+    return new Promise<void>((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
+
+async function changeLight() {
+    while (true) {
+        if (redLight && orangeLight && greenLight && redLight.classList.contains('red') ) {
+            await delay(2000);
+            addColor(orangeLight, trafficColor.REDORANGE);
+            /*await new Promise((res, rej) => {
+                setTimeout(res,2000);
+            });*/
+            await delay(2000);
+            addColor(greenLight, trafficColor.GREEN);
+            removeColor(redLight, trafficColor.RED)
+            removeColor(orangeLight, trafficColor.REDORANGE)
+            await delay(2000);
+            removeColor(greenLight, trafficColor.GREEN);
+            addColor(orangeLight, trafficColor.ORANGE);
+            await delay(2000);
+            removeColor(orangeLight, trafficColor.ORANGE);
+            addColor(redLight, trafficColor.RED);
+            await delay(2000);
+        }
+    }
+}
+
+function addColor( el:HTMLElement, color: trafficColor): void {
+    el.classList.add(color.toLowerCase());
+};
+
+function removeColor(el:HTMLElement, color:trafficColor):void {
+    el.classList.remove(color.toLowerCase());
+}
+
+function changecolor(el:HTMLElement, color:trafficColor) : void{
+    el.style.background = color.toLowerCase();
+}
+/*changeLight();*/
