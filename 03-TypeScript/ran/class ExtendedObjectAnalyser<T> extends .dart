e18abe23a6708aceecd89f9afdc14cf22bdcc98e
nishku84 <93193209+nishku84@users.noreply.github.com>
2024-06-05 16:45:23 +0000

    5 ביוני 2024, 19:45

--- 03-TypeScript/ran/class ExtendedObjectAnalyser<T> extends .dart
+++ 03-TypeScript/ran/class ExtendedObjectAnalyser<T> extends .dart
@@ -11,22 +11,13 @@ class ExtendedObjectAnalyser<T> extends ObjectAnalyser<T> {
     const values = Object.values(this.obj);
     let minValue: number | null = null;
 
-  }
-  getMinValue(): number | null {
-    if (typeof this.obj !== 'object' || this.obj === null) {
-      return null;
-    }
-
-    const values = Object.values(this.obj);
-    let minValue: number | null = null;
-
     for (const value of values) {
       if (typeof value === 'number') {
         if (minValue === null || value < minValue) {
           minValue = value;
         }
       } else if (typeof value === 'object' && value !== null) {
-        const nestedMin = new ExtendedObjectAnalyser(value).getMinValue();
+        const nestedMin = new ExtendedObjectAnalyser<any>(value).getMinValue();
         if (nestedMin !== null && (minValue === null || nestedMin < minValue)) {
           minValue = nestedMin;
         }
@@ -35,3 +26,30 @@ class ExtendedObjectAnalyser<T> extends ObjectAnalyser<T> {
 
     return minValue;
   }
+}
+
+const complexObject = {
+  a: { 
+    n: { 
+      key1: 6,
+      key2: {
+        key3: {
+          key4: 'my-str',
+        }
+      }
+    },
+    c: 6,
+    d: 9,
+  },
+  b: '8'
+};
+
+const extendedAnalyzer = new ExtendedObjectAnalyser(complexObject);
+const longestKey = extendedAnalyzer.getLongestKey();
+const objDepth = extendedAnalyzer.calculateObjDeep();
+const isConsistent = extendedAnalyzer.isValuesConsistent();
+const minValue = extendedAnalyzer.getMinValue();
+
+console.log('Longest Key:', longestKey);
+console.log('Object Depth:', objDepth);
+console.log('Values Consistent:', isCons​⬤
