2ff850cebb5bd86e061b83f1bdc37e17e3319e35
lorenabarlev <lorenabarlev@gmail.com>
2024-06-08 12:06:21 +0300

    class tasks

---
+++ 03-TypeScript/Lorena/HW 290524/dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+//check-if-a-number-is-palindrome
+function checkPalindrome(num) {
+    if (num <= 9) {
+        return true;
+    }
+    let strNum = String(num).split("");
+    const firstDig = Number(strNum.shift());
+    const lastDig = Number(strNum.pop());
+    const numWithoutFirstAndLastDigits = Number(strNum.join(""));
+    if (firstDig == lastDig) {
+        return checkPalindrome(numWithoutFirstAndLastDigits);
+    }
+    return false;
+}
+console.log(checkPalindrome(1002));
+//program-find-minimum-maximum-element-array
+function findMinMaxNumsInArr(arr, min = Infinity, max = -Infinity) {
+    if (arr.length <= 1) {
+        arr[0] > max ? (max = arr[0]) : "";
+        arr[0] < min ? (min = arr[0]) : "";
+        return { max: max, min: min };
+    }
+    arr[0] > max ? (max = arr[0]) : "";
+    arr[0] < min ? (min = arr[0]) : "";
+    const [removed, ...newArr] = arr;
+    return findMinMaxNumsInArr(newArr, min, max);
+}
+const arr = [10, 20, 11, 1111, 102412, 23];
+console.log(findMinMaxNumsInArr(arr));
