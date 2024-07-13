c3937ded9b3ae60981dcca5b9b31477efa37b96f
awnij28 <awni.n95@gmail.com>
2024-07-13 10:43:40 +0300

    React Folder & Test Project

---
+++ 05-React/Awni/my-app/src/project/HW/Hw1007.jsx
@@ -0,0 +1,10 @@
+/**
+ 2) Go over both my-app & my-add-advanced and understand:
+ a) different page components  
+ b) how to define routing between pages // using Routes and Route components from react-router-dom library  
+ c) how to use Link component in order to route between pages // using Link component from react-router-dom library  
+ d) how to import/export component from different modules (export default vs export) // using export default and export   
+ e) why we need sometimes <></> in a component // to wrap multiple elements in a single parent element  
+ f) how to use css in react // using css files and className attribute  
+ g) from previous lesson, make sure you know good about: component structure (props, js code, return statement) 
+ */
