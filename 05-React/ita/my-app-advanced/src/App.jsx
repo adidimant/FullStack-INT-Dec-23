5034c5b8ee451941fbf1dfda0474c580e6faea84
Ofer Ben Ami <132418948+OferBenA@users.noreply.github.com>
2024-07-21 11:25:24 +0300

    Merge branch 'main' of https://github.com/adidimant/FullStack-INT-Dec-23

---
+++ 05-React/ita/my-app-advanced/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import Home from './Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './NotFound'
+import Companies from './Companies';
+import CompanyDetails from './CompanyDetails';
+
+function App({ prop1, prop2, prop3 }) {
+  const a1 = 7;
+
+  if (a1 > 8) {
+    return (<div>No data</div>);
+  }
+
+  return (
+    <div className="App">
+        <BrowserRouter> 
+                <Routes> 
+                    <Route path="/" element={<Home prop1={prop1} prop2={prop2}/>} /> 
+                    <Route path="/companies" element={<Companies />} /> 
+                    <Route path="/companyDetails/:companyName" element={<CompanyDetails />} /> 
+                    <Route path="*" element={<NotFound />} />
+                </Routes> 
+        </BrowserRouter> 
+    </div>
+  );
+}
+
+export default App;
