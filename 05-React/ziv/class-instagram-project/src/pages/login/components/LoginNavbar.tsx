d57e6592dfe00f9487363ac2b051ba680bd2c5e0
zivelharar <ziv.elharar@gmail.com>
2024-07-31 16:53:24 +0300

    hw register 28/7

---
+++ 05-React/ziv/class-instagram-project/src/pages/login/components/LoginNavbar.tsx
@@ -0,0 +1,20 @@
+import { memo } from "react";
+import InstagramLogo from '../assets/instagram-text-logo.png';
+import { Link } from "react-router-dom";
+import Button from "../../../components/button/Button";
+
+function LoginNavbar() {
+  return (
+    <div className="login-page-navbar">
+        <div className="left-logo">
+          <Link to={'/'}><img src={InstagramLogo} alt="Instagram Logo" /></Link>
+        </div>
+        <div className="right-actions">
+          <Button name="login-btn" text="Log In" onClick={() => {}} />
+          <div className="navbar-signup-link"><Link to={'/register'} style={{ textDecoration: 'none'}}>Signup</Link></div>
+        </div>
+      </div>
+  );
+}
+
+export default memo(LoginNavbar);
