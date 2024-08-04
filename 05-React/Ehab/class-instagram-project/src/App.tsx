cca3269d9a33b2ee648c3185a2c335b2ea4c981b
asonhas <asonhas2@gmail.com>
2024-08-04 17:21:39 +0300

    ehab04082024-class-instagram-project

--- 05-React/Ehab/class-instagram-project/src/App.tsx
+++ 05-React/Ehab/class-instagram-project/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
+import Register from './pages/register/Register';
+import Reset from './pages/reset password/Reset';
 import './App.css';
 
 function App() {
@@ -8,6 +10,9 @@ function App() {
       <BrowserRouter> 
                 <Routes> 
                     <Route path="/" element={<LoginPage/>} /> 
+                    <Route path="/login" element={<LoginPage/>} /> 
+                    <Route path="/register" element={<Register/>} /> 
+                    <Route path="/reset-password" element={<Reset/>} /> 
                     <Route path='*' element={<></>}  />
                 </Routes> 
       </BrowserRouter> 
