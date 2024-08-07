44e29a035ef026e513aa1aa0ca603a7f2cdac4f2
Ofer Ben Ami <132418948+OferBenA@users.noreply.github.com>
2024-08-07 09:55:37 +0300

    finishing fetching story from api

--- 05-React/OferBenAmi/class-instagram-project/src/pages/post-auth-pages/components/Stories/Stories.tsx
+++ 05-React/OferBenAmi/class-instagram-project/src/pages/post-auth-pages/components/Stories/Stories.tsx
@@ -1,34 +1,26 @@
 import  { memo, useEffect, useState } from "react";
-import storyImg1 from '../../assets/story-img-1.jpg';
 import storyImg2 from '../../assets/profile.jpg';
-import profilePic3 from '../../assets/tweets.jpg'
 import Story from "./components/Story";
 import "./Stories.css"
 
 function Stories() {
-	const [userData, setUserData] = useState({});
+	const [userData, setUserData] = useState([]);
 
 	useEffect( () => {
-		fetch("https://randomuser.me/api/?results=10")
+		fetch("https://randomuser.me/api/?results=6")
 		.then(response  => response.json())
 		// .then(predata  => JSON.stringify(predata))
-		.then(data => {
-			setUserData(data)
-			const stringData = JSON.stringify(data.results)
-			console.log(`fetching is done, the data is: ${stringData}`)
-		})
+		.then(data => setUserData(data.results))
 	},[])
 
 
 
 	return (
 		<div className="Stories">
-			<Story username={userData.results[0].gender} profilePic={storyImg2}/>
-			<Story username='pishpeshuk' profilePic={storyImg1}/>
-			<Story username='tweetSrael' profilePic={profilePic3}/>
 			<Story username='ofer ben ami' profilePic={storyImg2}/>
-			<Story username='ofer ben ami' profilePic={storyImg2}/>
-
+			{userData ? userData.map((user: object, index: number) => {
+				return <Story username={user.name.first} profilePic={user.picture.thumbnail }/>
+			}) : <></>}
 		</div>
 	);
 }
