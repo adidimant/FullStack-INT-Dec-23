2d97b976afb82f07f28896c4a1051f1f86e19668
oritbg <oritbg@gmail.com>
2024-09-04 18:37:10 +0300

    hw file

--- 06-Server (nodejs)/Orit/index.js
+++ 06-Server (nodejs)/Orit/index.js
@@ -13,6 +13,26 @@ app.get('/loginn', (req, res) => {
   res.send('Loginn page!')
 })
 
+// 1. GET /about - returns a simple text response
+app.get('/about', (req, res) => {
+  console.log("New request from IP: " + req.ip);
+  res.send('This is the about page');
+});
+
+// 2. POST /submit - returns a JSON object
+app.post('/submit', (req, res) => {
+  console.log("New POST request from IP: " + req.ip);
+  const responseObject = { message: 'Form submitted successfully', status: 'OK' };
+  res.json(responseObject);
+});
+
+// 3. GET /data - returns an array of items as JSON
+app.get('/data', (req, res) => {
+  console.log("New request from IP: " + req.ip);
+  const dataArray = [1, 2, 3, 4, 5, { name: 'Item 6', value: 'Six' }];
+  res.json(dataArray);
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
