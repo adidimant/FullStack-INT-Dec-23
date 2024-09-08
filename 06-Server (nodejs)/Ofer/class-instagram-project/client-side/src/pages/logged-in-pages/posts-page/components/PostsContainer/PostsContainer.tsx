731b7d2c2506df088ddd554a763877a1802eccd0
Ofer Ben Ami <132418948+OferBenA@users.noreply.github.com>
2024-09-08 16:54:50 +0300

    finishing homework

--- 06-Server (nodejs)/Ofer/class-instagram-project/client-side/src/pages/logged-in-pages/posts-page/components/PostsContainer/PostsContainer.tsx
+++ 06-Server (nodejs)/Ofer/class-instagram-project/client-side/src/pages/logged-in-pages/posts-page/components/PostsContainer/PostsContainer.tsx
@@ -1,44 +1,63 @@
 import { memo, useCallback, useEffect, useState } from "react";
 import Post from "../Post/Post";
 import { RandomPostApiResult } from "../../../types";
-import './PostsContainer.css';
+import "./PostsContainer.css";
 
 function PostsContainer() {
-    const [posts, setPosts] = useState<RandomPostApiResult[]>([]); // setPosts is a function that updates the posts state variable with the new value passed to it as an argument (in this case, an array of posts) and triggers a re-render of the component.
+	const [posts, setPosts] = useState<RandomPostApiResult[]>([]); // setPosts is a function that updates the posts state variable with the new value passed to it as an argument (in this case, an array of posts) and triggers a re-render of the component.
 
-    const loadMorePosts = useCallback(async (amount: number) => {
-        try {
-            const response = await fetch('https://randomuser.me/api/?results=' + amount); // Fetch posts from the API.
-            const data = await response.json(); // Parse the response as JSON.
-            setPosts([...posts, ...data.results]); // Update the posts state variable with the fetched posts.
-            return data;
-        } catch (error) {
-            console.error('Error fetching posts:', error);
-        }
-    }, [posts]);
+	const controller = new AbortController();
 
-    useEffect(() => { // useEffect is a hook that runs a function when the component mounts and whenever the dependencies array changes.
-        if (posts.length < 50) { // critical for avoiding endless re-renders!
-            loadMorePosts(2);
-        }
-    }, [posts]);
+	const loadMorePosts = useCallback(
+		async (amount: number) => {
+			try {
+				const response = await fetch("http://localhost:3000/posts?results=" + amount,{ signal: controller.signal });
+				setTimeout(() => {
+					controller.abort();
+				}, 2000);
 
-    const handleRefresh = useCallback(() => { // Handle refresh button click. 
-        setPosts([]);
-    }, []);
+				if (controller.signal.aborted) {
+					throw new Error(`Response status: ${response.status}`);
+				}
+				const data = await response.json();
+				console.log(data);
+				setPosts([...posts, ...data.results]); // Update the posts state variable with the fetched posts.
+				return data;
+			} catch (error) {
+				console.error((error as Error).message);
+			}
+		},
+		[posts]
+	);
 
-    return (
-        <div className="postpage-container">
-           
-            <button className="postpage-btn" onClick={handleRefresh}>Refresh</button>
-            {posts.map((post: RandomPostApiResult, index: number) => (
-                <Post user={post.name.first}
-                    postImage={post.picture.large}
-                    likes={index * 10}
-                    timestamp={post.registered.date} />
-            ))}
-        </div>
-    );
+	useEffect(() => {
+		// useEffect is a hook that runs a function when the component mounts and whenever the dependencies array changes.
+		if (posts.length < 50) {
+			// critical for avoiding endless re-renders!
+			loadMorePosts(2);
+		}
+	}, [posts]);
+
+	const handleRefresh = useCallback(() => {
+		// Handle refresh button click.
+		setPosts([]);
+	}, []);
+
+	return (
+		<div className="postpage-container">
+			<button className="postpage-btn" onClick={handleRefresh}>
+				Refresh
+			</button>
+			{posts.map((post: RandomPostApiResult, index: number) => (
+				<Post
+					user={post.name.first}
+					postImage={post.picture.large}
+					likes={index * 10}
+					timestamp={post.registered.date}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default memo(PostsContainer);
