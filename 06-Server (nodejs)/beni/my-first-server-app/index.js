ca4853049af33df7913fef3fee697b1f8f0e8125
Benjamin <benjaminjaichenco@gmail.com>
2024-09-11 00:15:22 +0300

    submit hw

--- 06-Server (nodejs)/beni/my-first-server-app/index.js
+++ 06-Server (nodejs)/beni/my-first-server-app/index.js
@@ -3,12 +3,29 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  console.log(
-    `New request from ip: ${req.ip}, method: ${req.method}, endpoint: ${
-      req.url
-    }. headers: ${JSON.stringify(req.headers)}`
-  );
+  console.log(`New request from ip: ${req.ip}, method: ${req.method}, endpoint: ${req.url}.`);
   res.send("Hello World!");
+});
+
+app.get("/obj", (req, res) => {
+  console.log(`New request from ip:${req.ip}, method: ${req.method}, endpoint: ${req.url}.`);
+  const obj = {
+    first_name: "Beni",
+    last_name: "Jaichenco",
+  };
+  res.send(obj);
+});
+
+app.get("/arr", (req, res) => {
+  console.log(`New request from ip:${req.ip}, method: ${req.method}, endpoint: ${req.url}.`);
+  const arr = ["Beni", "Jaichenco"];
+  res.send(arr);
+});
+
+app.get("/json", (req, res) => {
+  console.log(`New request from ip:${req.ip}, method: ${req.method}, endpoint: ${req.url}.`);
+  const arrWithObj = [{ first_name: "Beni" }, { last_name: "Jaichenco" }];
+  res.json(arrWithObj);
 });
 
 app.listen(port, () => {
