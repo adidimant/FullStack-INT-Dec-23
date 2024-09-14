e2a7317baebdce1f52d8137cd54330f79582c99d
ita315 <itaraskin1234@gmail.com>
2024-09-14 21:45:49 +0300

    140924

--- 05-React/ita/instagram-project/server/guards/retelimit.js
+++ 05-React/ita/instagram-project/server/guards/retelimit.js
@@ -1,53 +1,41 @@
-const express = require('express');
-const { RateLimiterMemory } = require('rate-limiter-flexible');
-const Redis = require('ioredis');
-
-const redisClient = new Redis({ enableOfflineQueue: false });
+import { RateLimiterMemory } from 'rate-limiter-flexible';
 
 const rateLimiter1 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 5,  // 5 בקשות
     duration: 1  // שניה אחת
 });
 
 const rateLimiter2 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 8,  
     duration: 5  
 });
 
 const rateLimiter3 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 12,  
     duration: 10  
 });
 
 const rateLimiter4 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 15,  
     duration: 20  
 });
 
 const rateLimiter5 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 18,  
     duration: 30  
 });
 
 const rateLimiter6 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 20,  
     duration: 60  
 });
 
 const rateLimiter7 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 150,  
     duration: 1800  
 });
 
 const rateLimiter8 = new RateLimiterMemory({
-    storeClient: redisClient,
     points: 300,  
     duration: 3800  
 });
