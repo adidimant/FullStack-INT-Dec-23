6d88c0f9fb006549b11729e9f9755054ec81d050
asonhas <asonhas2@gmail.com>
2024-09-15 18:00:18 +0300

    ehab-HW110924

--- 06-Server (nodejs)/ehab/class-instagram-project-server/guards/rateLimiting.js
+++ 06-Server (nodejs)/ehab/class-instagram-project-server/guards/rateLimiting.js
@@ -1,62 +1,59 @@
 
 const requestLog = {}; // This will hold request logs in memory
 
-const getTime = (seconds)=>{
-    return seconds * 1000;
-}
 export const rateLimitingMiddleware = (req, res, next) => {
     const key = `${req.ip}:${req.headers['user-agent']}`;
     const now = Date.now();
     if (!requestLog[key]) {
         requestLog[key] = [];
     }
+    // Log the current request timestamp
+    requestLog[key].push(now);
     
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(1));
-    console.log(requestLog[key].length)
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < 1);
     if(requestLog[key].length > 5){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
 
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(5));
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < 5);
     if(requestLog[key].length > 8){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(10));
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < 10);
     if(requestLog[key].length > 12){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(20));
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < 20);
     if(requestLog[key].length > 15){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(30));
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < 30);
     if(requestLog[key].length > 18){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(60));
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < 60);
     if(requestLog[key].length > 20){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(1800));
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) <1800);
     if(requestLog[key].length > 150){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
-    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < getTime(3600));
+    requestLog[key] = requestLog[key].filter(timestamp => (now - timestamp) < 3600);
     if(requestLog[key].length > 300){
         res.status(429).send('Too many requests. Please try again later.');
         return;
     }
     
 
-    // Log the current request timestamp
-    requestLog[key].push(now);
+    
     
     next();
 };
