adba2df8a0ec89e27279f13e221617586303771b
adidimant <adi.dimant@transmitsecurity.com>
2024-09-15 20:38:30 +0300

    Add rate-limiting, convert project to TS

---
+++ class-instagram-project/leftovers.md
@@ -0,0 +1,13 @@
+1) Convert server to TS
+2) Views - present customized views in case of 500 / 404 errors
+3) Database, for: users, posts, activities/audit logs?
+  - models for users, posts
+4) APIs (endpoints)
+5) Registration
+6) Login - authentication mechanisem
+  - Authorization - the ability to perform user operations after the authentication
+7) Optional features:
+  - likes
+  - followers
+  - share
+  - comments
--- class-instagram-project/server/src/controllers/posts.router.ts
+++ class-instagram-project/server/src/controllers/posts.router.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import axios from 'axios';
 const postsRouter = express.Router();
 
-postsRouter.get('/', (req, res) => {
-  throw new Error();
+postsRouter.get('/', async (req, res) => {
   console.log(`New request from ip: ${req.ip}, method: ${req.method}, endpoint: ${req.url}. headers: ${JSON.stringify(req.headers)}`);
   const { results } = req.query;
   if (results > 100) {
@@ -11,9 +10,9 @@ postsRouter.get('/', (req, res) => {
     return;
   }
   try {
-    // const response = await axios.get('https://randomuser.me/api/?results=' + results); // Fetch posts from the API.
-    // const data = response.data;
-    // res.json(data.results);
+    const response = await axios.get('https://randomuser.me/api/?results=' + results); // Fetch posts from the API.
+    const data = response.data;
+    res.json(data.results);
   } catch (err) {
     console.error('Error fetching data:', err);
     res.status(500).json({ error: 'Failed to fetch posts' });
