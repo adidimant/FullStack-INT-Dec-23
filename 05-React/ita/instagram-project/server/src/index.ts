d26e609b5938d7e0ba3c5fb853425ddaf388b7d8
ita315 <itaraskin1234@gmail.com>
2024-09-22 17:26:05 +0300

    220924

--- 05-React/ita/instagram-project/server/src/index.ts
+++ 05-React/ita/instagram-project/server/src/index.ts
@@ -1,19 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import postsRouter from './controllers/posts.router.js';  
-import { rateLimitMiddleware } from './middlewares/rate-limit.js';
-
+import mongoose, { mongo } from 'mongoose';
+import postsRouter from './controllers/posts.router';
+import { rateLimitMiddleware } from './middlewares/rate-limit';
 
 const app = express();
 const port = 3000;
 
-const platformLogMiddleware = (req, res, next) => {
+mongoose.connect('mongodb+srv://itaraskin1234:qTKuKqQdHYh7Miyr@ita-instagram-project.2rubd.mongodb.net/?retryWrites=true&w=majority&appName=ita-instagram-project').then(() => {
+  console.log("MongoDB is connected!");
+}).catch((err) => console.error(err));
+
+const platformLogMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const userAgent = req.headers["user-agent"];
   console.log('userAgent: ', userAgent);
   next();
 };
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const num = Math.random()*100;
   if (num > 1) {
     next();
@@ -21,6 +25,9 @@ const authMiddleware = (req, res, next) => {
   }
   res.status(401).send('Unauthorized! please log in!');
 };
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static("./views/assets"));
 
 app.use(rateLimitMiddleware);
 app.use(platformLogMiddleware);
@@ -31,6 +38,7 @@ app.use(cors({
 }));
 
 app.use('/api/posts', authMiddleware, postsRouter);
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
