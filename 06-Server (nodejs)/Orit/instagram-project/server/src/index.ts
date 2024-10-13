025d51f25c60a64e0dc0265408a24859b4b76642
oritbg <oritbg@gmail.com>
2024-10-13 19:32:12 +0300

    old files

--- 06-Server (nodejs)/Orit/instagram-project/server/src/index.ts
+++ 06-Server (nodejs)/Orit/instagram-project/server/src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose, { mongo } from 'mongoose';
+//import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import postsRouter from './controllers/posts.router';
 import { rateLimitMiddleware } from './middlewares/rate-limit';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
-mongoose.connect('my-url').then(() => {
+//mongoose.connect('my-url').then(() => {
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/';
+mongoose.connect(mongoURI).then(() => {
   console.log("MongoDB is connected!");
 }).catch((err) => console.error(err));
 
@@ -27,7 +32,25 @@ const authMiddleware = (req: express.Request, res: express.Response, next: expre
 };
 
 app.set('view engine', 'ejs');
+app.set('views', './views');
 app.use('/public', express.static("./views/assets"));
+
+app.get('/', (req, res) => {
+  // Create an error on every page.
+  throw new Error('Something went wrong!');
+});
+
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack); // Log the error
+  res.status(500).render('failed'); // Render the 'failed' view for status 500 errors
+});
+
+/*const errorHandler: express.ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('failed');
+};
+
+app.use(errorHandler);*/
 
 app.use(rateLimitMiddleware);
 app.use(platformLogMiddleware);
