ce5f7329f04dab8bbad87d665389c363f4b19a32
adidimant <adi.dimant@transmitsecurity.com>
2024-12-19 23:06:29 +0200

    Include acceptance hour context

--- 07-SQL/extra-recording.sql
+++ 07-SQL/extra-recording.sql
@@ -1,12 +1,12 @@
 
 -- extra recording:
 -- 1) using SQL to query database from the backend: https://node-postgres.com/
--- 2) fields types (varchar, boolean,date...)
--- 3) primary key, auto increment
-
+-- 2) fields types (varchar, boolean,date...) - https://www.w3schools.com/sql/sql_datatypes.asp
+-- 3) primary key, auto increment - https://www.w3schools.com/sql/sql_primarykey.asp
+ -- other constrains - https://www.w3schools.com/sql/sql_constraints.asp
 -- 4) TABLE operations
-CREATE TABLE
-ALTER TABLE add column1 VARCHAR set default 'true'
+-- CREATE TABLE - https://www.w3schools.com/sql/sql_create_table.asp
+-- ALTER TABLE - https://www.w3schools.com/sql/sql_alter.asp
 
 const client = new Client({ url: <>, port: 5432, username: 'aa', password: '<password>'})
 
