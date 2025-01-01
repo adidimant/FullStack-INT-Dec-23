ffc2bdc094ea49855e54573e8c0b2eae5b3af643
adidimant <adi.dimant@transmitsecurity.com>
2025-01-01 20:53:42 +0200

    Add react-native content

---
+++ 08-React-Native/react-native-ios-action-sheet.tsx
@@ -0,0 +1,50 @@
+// https://reactnative.dev/docs/actionsheetios
+
+import React, {useState} from 'react';
+import {ActionSheetIOS, Button, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => {
+  const [result, setResult] = useState('🔮');
+
+  const onPress = () =>
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', 'Generate number', 'Reset'],
+        destructiveButtonIndex: 2,
+        cancelButtonIndex: 0,
+        userInterfaceStyle: 'dark',
+      },
+      buttonIndex => {
+        if (buttonIndex === 0) {
+          // cancel action
+        } else if (buttonIndex === 1) {
+          setResult(String(Math.floor(Math.random() * 100) + 1));
+        } else if (buttonIndex === 2) {
+          setResult('🔮');
+        }
+      },
+    );
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.result}>{result}</Text>
+        <Button onPress={onPress} title="Show Action Sheet" />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  result: {
+    fontSize: 64,
+    textAlign: 'center',
+  },
+});
+
+export default App;
